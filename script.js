9ebaaa86fe164d4f15476b28b51d34ecfff4f4fa
antonysimos <aseemos8@gmail.com>
2022-03-14 17:43:43 -0400

    initial_commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let colorSelected; 
+let colorSelected;
 
 //Adds a row
 function addR() {
@@ -6,7 +6,7 @@ function addR() {
     let grid = document.getElementById("grid");
     let rows = document.getElementsByTagName("tr");
     console.log(rows.length);
-    
+
     if (rows.length === 0) {
         let row = document.createElement("tr");
         let col = document.createElement("td");
@@ -18,11 +18,11 @@ function addR() {
 
     }
 }
-//Adds a column
+//Adds a column (:
 function addC() {
     //alert("Clicked Add Col")
     let cols = document.getElementsByTagName("td");
-    console.log(cols.length); 
+    console.log(cols.length);
 }
 
 //Removes a row
@@ -50,4 +50,3 @@ function clearAll(){
 function fillU(){
     alert("Clicked Fill All Uncolored")
 }
-
