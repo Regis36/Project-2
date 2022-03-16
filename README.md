31feb882ad0a4abfb550e684bc56069999b3c959
Regis Enama <Regis.Enama82@myhunter.cuny.edu>
2022-03-16 01:00:48 -0400

    added new things to the README

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Antony Simos!
 Regis Enama!
 Blaise Enama!
+
+Regis:
+- did the removeR and removeC functions 
+
+Antony:
+-did the addR and addC functions 
+
+Blaise:
+-did the fill and clearAll functions 
--- script.js
+++ script.js
@@ -6,18 +6,23 @@ function addR() {
     //alert("Clicked Add Row")
     let grid = document.getElementById("grid");
     let rows = document.getElementsByTagName("tr");
+      let gridrows = grid.rows.length;
+      let row = document.createElement("tr"); //add a row to the grid 
+        let col = document.createElement("td");
     console.log(rows.length);
 
     if (rows.length === 0) {
         let row = document.createElement("tr");
         let col = document.createElement("td");
         col.onclick = function(){
+
             this.style.backgroundColor = colorSelected;
         };
+        
         row.appendChild(col);
         grid.appendChild(row);
-
-    }
+    } 
+    
     else {
       let row = document.createElement("tr");
       let col = document.createElement("td");
@@ -29,22 +34,40 @@ function addR() {
     }
 
 }
+
 //Adds a column
 //function done by Blaise 
 function addC() {
     //alert("Clicked Add Col")
     let cols = document.getElementsByTagName("td");
-    console.log(cols.length);
+    let grid = document.getElementById("grid"); 
+    let rows = document.getElementsByTagName("tr");
+
+    for (let i = 0; i < rows.length; i++)
+    {
+        let col = document.createElement("td"); 
+        let row = document.createElement("tr"); 
+
+        col.onclick = function(){
+          this.style.backgroundColor = colorSelected;
+      };
+
+        rows[i].appendChild(col);
+    }
 }
 
 //Removes a row
 //function done by Regis 
 function removeR() {
-    alert("Clicked Remove Row")
+    // alert("Clicked Remove Row")
+    grid.deleteRow(0); 
 }
 //Remove a column
 function removeC() {
-    alert("Clicked Remove Col")
+    let rows = document.getElementsByTagName("tr"); 
+    for (let i = 0; i < rows[0].cells.length; i++) {
+        rows[i].deleteCell(i); 
+    }
 }
 //sets global var for selected color
 function selected(){
@@ -58,6 +81,13 @@ function fill(){
 
 function clearAll(){
     alert("Clicked Clear All")
+    let grid = document.querySelectorAll("grid"); 
+    grid.onclick = function(){
+        this.style.backgroundColor = ' ';
+        // if(grid === colorSelected ){
+        //     this.style.backgroundColor = white; 
+        // }
+    };
 }
 
 function fillU(){
